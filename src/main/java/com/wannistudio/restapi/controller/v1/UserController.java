36fa5ad4368c278cb355bbffae98c8594a23d2b8
wanni0928 <chlgudwnd123@gmail.com>
2021-02-02 15:10:21 +0900

    feat: add swagger

--- src/main/java/com/wannistudio/restapi/controller/v1/UserController.java
+++ src/main/java/com/wannistudio/restapi/controller/v1/UserController.java
@@ -2,30 +2,35 @@ package com.wannistudio.restapi.controller.v1;
 
 import com.wannistudio.restapi.domain.user.User;
 import com.wannistudio.restapi.repository.UserJpaRepo;
+import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiOperation;
+import io.swagger.annotations.ApiParam;
 import lombok.RequiredArgsConstructor;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
+@Api(tags = {"1. User"})
 @RequiredArgsConstructor
-@RestController // 결과값을 JSON으로 출력합니다.
+@RestController
 @RequestMapping(value = "/v1")
 public class UserController {
+
     private final UserJpaRepo userJpaRepo;
 
+    @ApiOperation(value = "회원 조회", notes = "모든 회원을 조회한다")
     @GetMapping(value = "/user")
     public List<User> findAllUser() {
         return userJpaRepo.findAll();
     }
 
+    @ApiOperation(value = "회원 입력", notes = "회원을 입력한다.")
     @PostMapping(value = "/user")
-    public User save() {
+    public User save(@ApiParam(value = "회원아이디", required = true) @RequestParam String uid,
+                     @ApiParam(value = "회원이름", required = true) @RequestParam String name) {
         User user = User.builder()
-                .uid("wanni@naver.com")
-                .name("wanni")
+                .uid(uid)
+                .name(name)
                 .build();
         return userJpaRepo.save(user);
     }
